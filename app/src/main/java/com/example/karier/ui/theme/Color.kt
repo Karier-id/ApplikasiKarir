23c4592a5b31f724a0c0b97b582c5fc51be61df0
rafikaWardah12 <rafika.wardah12@gmail.com>
2024-05-17 14:32:51 +0700

    Feat: Login Screen

--- app/src/main/java/com/example/karier/ui/theme/Color.kt
+++ app/src/main/java/com/example/karier/ui/theme/Color.kt
@@ -2,10 +2,22 @@ package com.example.karier.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+val PrimaryBackground =  Color(0xFFEBF2F5)
+val SecondaryBackground = Color(0xFFF5FCFF)
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+//val PrimaryBlue500 = Color(0xFF154752)
+val PrimaryBlue400 = Color(0xFF0CB9F2)
+val PrimaryBlue300 = Color(0xFF87CEEB)
+val PrimaryBlue200 = Color(0xFFBEE5F5)
+val PrimaryGreen500 = Color(0xFF1E6874)
+val OnPrimaryBlue = Color.White
+
+val TextPrimary = Color(0xFF15434E)
+val TextSecondary = Color(0xFF868484)
+val TextTertiary = Color(0xFF676666)
+val TextDisable = Color(0xFFB7B7B7)
+val Outline = Color(0xFFA3A3A3)
+val OutlineFocused = Color(0xFFDBDADA)
+
+val ContainerPrimary = Color.White
+val ContainerDisable = Color(0xFFF6F6F6)
